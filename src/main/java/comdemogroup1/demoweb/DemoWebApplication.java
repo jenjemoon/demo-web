1f0779fd99c62cca86faad21c432a57396311bf0
GilbertMoon <55611811+GilbertMoon@users.noreply.github.com>
2023-03-17 18:07:43 +0900

    demo-web

---
+++ src/main/java/comdemogroup1/demoweb/DemoWebApplication.java
@@ -0,0 +1,13 @@
+package comdemogroup1.demoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebApplication.class, args);
+	}
+
+}
